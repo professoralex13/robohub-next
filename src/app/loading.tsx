b81b37ab3ab0da49c8f849852e4ec8fe51f0f68d
Alex Cutforth <alex@densem.net>
2023-05-15 20:13:51 +1200

    Create loading.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { Oval } from 'react-loading-icons';
+
+const Loading = () => (
+    <div className="h-screen w-screen">
+        <Oval className="absolute inset-0 m-auto" />
+    </div>
+);
+
+export default Loading;
