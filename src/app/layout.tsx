6b32e38fc5080e028adaca84e93e4847218dd580
Alex Cutforth <alex@densem.net>
2023-05-18 09:03:37 +1200

    fix: use imported font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,34 @@ import { Header } from '@/components/Header';
 import { Session } from 'next-auth';
 import Providers from '@/components/Providers';
 import { Analytics } from '@vercel/analytics/react';
+import localFont from 'next/font/local';
 
+const sans = localFont({
+    src: [
+        {
+            path: '../fonts/Product Sans Regular.ttf',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../fonts/Product Sans Italic.ttf',
+            weight: '400',
+            style: 'italic',
+        },
+        {
+            path: '../fonts/Product Sans Bold.ttf',
+            weight: '700',
+            style: 'normal',
+        },
+        {
+            path: '../fonts/Product Sans Bold Italic.ttf',
+            weight: '700',
+            style: 'italic',
+        },
+    ],
+});
 const RootLayout: FC<PropsWithChildren<{ session: Session }>> = ({ session, children }) => (
-    <html lang="en">
+    <html lang="en" className={sans.className}>
         <body>
             <Providers session={session}>
                 <Header />
